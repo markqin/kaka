72b5cbd13ba51a3a3d79275a69e29bb0ab2af5da
SoloistWang <342916053@qq.com>
2016-06-20 19:12:51 +0800

    kaka update auto

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-const electron = require('electron');
-// Module to control application life.
-const {app} = electron;
-// Module to create native browser window.
-const {BrowserWindow} = electron;
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let win;
-
+var app = require('app');  // 控制应用生命周期的模块。
+var BrowserWindow = require('browser-window');  // 创建原生浏览器窗口的模块
 
 //Squirrel
 var path = require('path');
 var cp = require('child_process');
+
+var handleSquirrelEvent = function() {
+   if (process.platform != 'win32') {
+      return false;
+   }
+
+  
+  var checkUpdate = function(){
+  var updateDotExe = path.resolve(path.dirname(process.execPath),'..', 'update.exe');
+  var child = cp.spawn(updateDotExe,["--update", "https://tonytony.club/tool/kaka/release/"], { detached: true });
+  child.on('close', function(code) {
+      // alert('KAKA版本更新完成！重启体验新版本！');
+      // anything you need to do when update is done.
+  }); 
+}
+
 
   function executeSquirrelCommand(args, done) {
       var updateDotExe = path.resolve(path.dirname(process.execPath), 
@@ -22,7 +30,7 @@ var cp = require('child_process');
       });
   };
 
-  function install(done) {
+   function install(done) {
       var target = path.basename(process.execPath);
       executeSquirrelCommand(["--createShortcut", target], done);
    };
@@ -31,11 +39,7 @@ var cp = require('child_process');
       var target = path.basename(process.execPath);
       executeSquirrelCommand(["--removeShortcut", target], done);
    };
-
-var handleSquirrelEvent = function() {
-   if (process.platform != 'win32') {
-      return false;
-   }
+    checkUpdate();
    var squirrelEvent = process.argv[1];
    switch (squirrelEvent) {
       case '--squirrel-install':
@@ -55,63 +59,54 @@ var handleSquirrelEvent = function() {
    return false;
 };
 
+
+
 if (handleSquirrelEvent()) {
+  
+  app.quit();
    return;
 }
 //end Squirrel
 
 
 
+// 保持一个对于 window 对象的全局引用，不然，当 JavaScript 被 GC，
+// window 会被自动地关闭
+var mainWindow = null;
 
-
-function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow({
-    width: 800,
-    // width: 1200,
-    height: 750,
-    minWidth: 650,
-    minHeight: 750,
-    // alwaysOnTop: true,
-    autoHideMenuBar: true
-  });
-
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/index.html`);
-
-  // Open the DevTools.
-  // win.webContents.openDevTools();
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+// 当所有窗口被关闭了，退出。
+app.on('window-all-closed', function() {
+  // 在 OS X 上，通常用户在明确地按下 Cmd + Q 之前
+  // 应用会保持活动状态
+  if (process.platform != 'darwin') {
     app.quit();
   }
 });
 
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow();
-  }
-});
+// 当 Electron 完成了初始化并且准备创建浏览器窗口的时候
+// 这个方法就被调用
+app.on('ready', function() {
+  // 创建浏览器窗口。
+  mainWindow = new BrowserWindow({
+    width: 920,
+    // width: 1200,
+    height: 750,
+    minWidth: 650,
+    minHeight: 750,
+    autoHideMenuBar: true
+  });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+  // 加载应用的 index.html
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // 打开开发工具
+  // mainWindow.openDevTools();
+
+  // 当 window 被关闭，这个事件会被发出
+  mainWindow.on('closed', function() {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 但这次不是。
+    mainWindow = null;
+  });
+});
